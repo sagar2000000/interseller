49232b9d8a40c512b3c6f93092d475bed0fb53a3
Gyrod Gaming <sagar171joshi@gmail.com>
2023-12-06 12:59:47 +0545

    Niii

---
+++ index.js
@@ -0,0 +1,17 @@
+
+       
+        let currentSlide = 0;
+        const slides = document.querySelectorAll('.slideshow img');
+
+        function showSlide(index) {
+            slides.forEach((slide, i) => {
+                slide.style.display = i === index ? 'block' : 'none';
+            });
+        }
+
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % slides.length;
+            showSlide(currentSlide);
+        }
+
+        setInterval(nextSlide, 3000);
